2ad9cdb473ada795466303097244821582594ce6
Adit <32681526+Aditmadzs@users.noreply.github.com>
2018-07-31 07:28:32 +0700

    Update Aditmadzs.py

--- Aditmadzs.py
+++ Aditmadzs.py
@@ -19,7 +19,7 @@ try:
 						with open("authToken.txt","w") as token:
 							token.write(client.authToken)
 						continue
-					client = LINE(authToken, speedThrift=False, appName="{}\t2.1.5\tHelloWorld\t11.2.5".format(app))
+					client = LINE(authToken, speedThrift=False, appName="{}\t2.1.5\tAditmadzs\t11.2.5".format(app))
 				break
 			except Exception as error:
 				print(error)
